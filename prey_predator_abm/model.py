ae35df4a7adef2488e3dfa849a1162f029023ccd
Danillo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-12-13 19:53:43 -0300

    working example

--- prey_predator_abm/model.py
+++ prey_predator_abm/model.py
@@ -298,21 +298,21 @@ def grow_food(params, substep, state_history, prev_state):
 
 
 initial_values = {
-    'world_size_n_dimension': 20,
-    'world_size_m_dimension': 20,
-    'initial_food_sites': 5,
-    'initial_predator_count': 20,
-    'initial_prey_count': 20
+    'world_size_n_dimension': 16,
+    'world_size_m_dimension': 16,
+    'initial_food_sites': 4,
+    'initial_predator_count': 16,
+    'initial_prey_count': 64
 }
 
 params = {
     'food_growth_rate': [3],
     'maximum_food_per_site': [10],
     'reproduction_probability': [1.0],
-    'agent_lifespan': [35],
-    'reproduction_food_threshold': [6],  # When the agents start reproducing
-    'hunger_threshold': [15],  # When the agents start eating
-    'reproduction_food': [1]  # How much food is needed for reproducing
+    'agent_lifespan': [50],
+    'reproduction_food_threshold': [8],  # When the agents start reproducing
+    'hunger_threshold': [20],  # When the agents start eating
+    'reproduction_food': [2]  # How much food is needed for reproducing
 }
 
 initial_state = {
@@ -402,5 +402,5 @@ partial_state_update_block = [
     }
 ]
 
-N_samples = 20
-N_timesteps = 300
+N_samples = 50
+N_timesteps = 50
