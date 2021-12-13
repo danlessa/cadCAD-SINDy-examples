68395e2b182b664c302756f0b204325bb909894d
Danillo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-12-13 17:01:53 -0300

    initial commit

---
+++ prey_predator_abm/__init__.py
@@ -0,0 +1,8 @@
+from prey_predator_abm.model import initial_state, params, partial_state_update_block
+from prey_predator_abm.model import N_timesteps, N_samples
+
+DEFAULT_ARGS = (initial_state,
+                params,
+                partial_state_update_block,
+                N_timesteps,
+                N_samples)
